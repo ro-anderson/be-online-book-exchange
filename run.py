803377d85bb8e822f4a7520d7ed0db1174611259
Rodrigo Anderson <rodrigo.anderson@emcasa.com>
2023-05-28 20:22:12 -0300

    chore: add PORT variable for heroku deploy

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from src.main.configs import app
+from os import environ
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host='0.0.0.0', port=environ.get('PORT', 5001))
